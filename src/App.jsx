b57992da50338378db5c5385df8722142026d9d5
dataanalyst1-cpu <data.analyst1@ribshackcorp.com>
2025-12-29 09:46:07 +0800

    App.jsx Update
    - Reverted to error free version

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useNavigate, useLocation, Navigate } from 'react-router-dom';
+import { apiEndpoints } from './services/api';
 import './App.css';
 
 // --- ICONS & ASSETS IMPORT ---
