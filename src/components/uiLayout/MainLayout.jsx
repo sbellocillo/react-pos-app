343087082a623f10c27ee59362c33102390cebfd
dataanalyst1-cpu <data.analyst1@ribshackcorp.com>
2026-01-22 11:51:08 +0800

    Login Update
    - New login system using AuthContext
    - Store acc info in browser storage

--- src/components/uiLayout/MainLayout.jsx
+++ src/components/uiLayout/MainLayout.jsx
@@ -34,7 +34,7 @@ export default function MainLayout({ children }) {
   const handleLogout = () => {
     localStorage.removeItem('authToken');
     localStorage.removeItem('currentUser');
-    navigate('/login');
+    navigate('/');
   };
 
   const getPageTitle = (path) => {
